0bd75f9c23a290c3a4c910101f5e46d6fe6f8ff7
Dellybizz <asmazuberi7@gmail.com>
2026-09-18 04:07:33 +0530

    Import page-runtime-hook.js

---
+++ page-runtime-hook.js
@@ -0,0 +1,11 @@
+(()=>{
+  const previousWrite=document.write.bind(document);
+  document.write=function(html){
+    if(typeof html==='string'&&!html.includes('Birthday Site Control Room')){
+      const script='<script src="./page-patch-runtime.js?v=15"></'+'script>';
+      const i=html.toLowerCase().lastIndexOf('</body>');
+      html=i>=0?html.slice(0,i)+script+html.slice(i):html+script;
+    }
+    return previousWrite(html);
+  };
+})();
